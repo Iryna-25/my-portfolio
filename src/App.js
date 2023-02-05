a5e8737669e72004c6a839b0d09b681e2c107d4c
Iryna Shandar <iryna.shandar@gmail.com>
2023-02-05 21:11:22 +0200

    Skills/Projects/Contacts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import { NavBar } from "./components/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
 import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Contacts } from './components/Contacts';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Skills/>
+      <Projects/>
+      <Contacts/>
     </div>
   );
 }
